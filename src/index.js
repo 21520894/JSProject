ffcfa26c170b43dda9ec7da9d7f68b44b4bc7e92
Duy Hung <21520894@gm.uit.edu.vn>
2024-09-20 16:07:24 +0700

    install sequelize

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWebRoute from "./routes/web";
+require("dotenv").config();
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+viewEngine(app);
+initWebRoute(app);
+
+let port = process.env.PORT || 6969;
+
+app.listen(port, () => {
+	console.log("Nodejs is running on", port);
+});
